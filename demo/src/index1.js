88cdf6cf6339b0c9925aa8c72bea8711f8dbd46d
Hung Viet Nguyen <henry@gotitapp.co>
2018-09-22 17:44:44 +0700

    Fix demo.

--- demo/src/index1.js
+++ demo/src/index1.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
+import QueryString from 'query-string';
 import { LinkedInPopUp } from '../../src';
 import LinkedInPage from './LinkedInPage';
 
 class Demo1 extends Component {
   render() {
-    const { params } = this.state;
+    const params = QueryString.parse(window.location.search);
     if (params.code || params.error) {
       return (
         <LinkedInPopUp />
